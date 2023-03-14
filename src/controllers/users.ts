a20c915fb142b8eeb557b9d624069dead79d059f
opedrodev <pvasilva.dev@hotmail.com>
2023-03-14 18:38:12 -0300

    refactor: remove try catch block

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,15 +3,9 @@ import { createToken } from '../auth';
 import UserService from '../services/users';
 
 async function addUser(req: Request, res: Response) {
-  try {
-    await UserService.addUser(req.body);
-    const token = createToken(req.body);
-    res.status(201).json({ token });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
+  await UserService.addUser(req.body);
+  const token = createToken(req.body);
+  res.status(201).json({ token });
 }
 
 const UserController = {
